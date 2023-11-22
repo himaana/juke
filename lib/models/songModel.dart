6113480ea61ef5dc755e034146ee883be9c5d3a2
Himana <himana.on@gmail.com>
2023-11-23 05:30:29 +0700

    Create Song class

---
+++ lib/models/songModel.dart
@@ -0,0 +1,15 @@
+class Song {
+  final String artis;
+  final String title;
+  final String genre;
+  final String url;
+  final String coverUrl;
+
+  Song({
+    required this.artis,
+    required this.title,
+    required this.genre,
+    required this.url,
+    required this.coverUrl,
+  });
+}

a664f1830f7f9c3f388fb83dd20b91e30e816120
Himana <himana.on@gmail.com>
2023-11-26 17:45:55 +0700

    routes of the app

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,19 +1,23 @@
 import 'package:get/get.dart';
 import 'package:juke/screens/screens.dart';
 
-class AppPage {
-  static List<GetPage> routre = [
+class AppRoutes {
+  static List<GetPage> routes = [
     GetPage(name: navbar, page: () => const NavScreen()),
     GetPage(name: home, page: () => const HomeScreen()),
     GetPage(name: library, page: () => const LibraryScreen()),
-    GetPage(name: search, page: () => const SearchScreen())
+    GetPage(name: search, page: () => const SearchScreen()),
+    GetPage(
+        name: song, page: () => const SongScreen(), arguments: Get.arguments),
   ];
 
   static getnavbar() => navbar;
   static gethome() => home;
   static getlibrary() => library;
   static getsearch() => search;
+  static getsong() => song;
 
+  static String song = '/song';
   static String navbar = '/';
   static String home = '/home';
   static String library = '/library';
